c890db4ff91e1ad790a98ad8b6d4c9c12950060b
TheThreeBody <29009714+TheThreeBody@users.noreply.github.com>
2021-10-20 16:23:00 +0800

    second

--- src/utils/realURLsAPI.js
+++ src/utils/realURLsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://www.npsex.com:9999'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://www.npsex.com:8888'
 
 let token = localStorage.token
 
